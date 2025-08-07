160bce11400fdae7f074b4102eeb5e21d04feebf
Nera Markovic <nera.markovic@gmail.com>
2025-08-07 12:48:23 +0200

    Add UserManager class with add and remove methods

---
+++ user_manager.py
@@ -0,0 +1,14 @@
+from user import User
+
+class UserManager:
+    def __init__(self):
+        self.users = []
+
+    def add_user(self, user):
+        self.users.append(user)
+            def remove_user(self, user_id):
+        for user in self.users:
+            if user.id == user_id:
+                self.users.remove(user)
+                return True
+        return False
